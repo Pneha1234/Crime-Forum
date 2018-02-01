b3c28a3e036c0008250781ef1e378ab82531d333
Neha408 <nehapandey@sxc.edu.np>
2018-02-01 20:23:04 +0545

    initial commit added answer to the site

---
+++ view_topic.php
@@ -0,0 +1,129 @@
+<?php 
+$host="localhost"; // Host name 
+$username="root"; // Mysql username 
+$password="nehapandey1234"; // Mysql password 
+$db_name="myforum"; // Database name 
+$tbl_name="fquestions"; // Table name 
+ini_set('display_errors', 1);
+
+ 
+// Connect to server and select databse.
+$conn=mysqli_connect($host, $username, $password,$db_name)or die("cannot connect"); 
+//mysql_select_db("$db_name")or die("cannot select DB");
+ 
+// get value of id that sent from address bar 
+$id=$_GET['id'];
+$sql="SELECT * FROM ".$tbl_name." WHERE id='".$id."'";
+$result=mysqli_query($conn,$sql);
+$rows=mysqli_fetch_array($result);
+?>
+
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<td><table width="100%" border="0" cellpadding="3" cellspacing="1" bordercolor="1" bgcolor="#FFFFFF">
+<tr>
+<td bgcolor="#F8F7F1"><strong><?php echo $rows['topic']; ?></strong></td>
+</tr>
+ 
+<tr>
+<td bgcolor="#F8F7F1"><?php echo $rows['detail']; ?></td>
+</tr>
+<tr>
+<td bgcolor="#F8F7F1"><strong>By :</strong> <?php echo $rows['name']; ?></td>
+<tr>
+<td>
+ <strong>Email : </strong><?php echo $rows['email'];?></td>
+</tr>
+ 
+<tr>
+<td bgcolor="#F8F7F1"><strong>Date/time : </strong><?php echo $rows['datetime']; ?></td>
+</tr>
+</table></td>
+</tr>
+</table>
+
+<?php
+ 
+$tbl_name2="fanswer"; // Switch to table "forum_answer"
+$sql2="SELECT * FROM ".$tbl_name2." WHERE question_id='".$id."'";
+$result2=mysqli_query($conn,$sql2);
+
+while($rows=mysqli_fetch_array($result2)){ 
+	
+
+?>
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr>
+<td><table width="100%" border="0" cellpadding="3" cellspacing="1" bordercolor="1" bgcolor="#FFFFFF">
+<tr>
+<td bgcolor="#F8F7F1"><strong><?php echo $rows['a_answer']; ?></strong></td>
+</tr>
+ 
+
+<tr>
+<td bgcolor="#F8F7F1"><strong>By :</strong> <?php echo $rows['a_name']; ?></td>
+<tr>
+<td>
+ <strong>Email : </strong><?php echo $rows['a_email'];?></td>
+</tr>
+ 
+<tr>
+<td bgcolor="#F8F7F1"><strong>Date/time : </strong><?php echo $rows['a_datetime']; ?></td>
+</tr>
+</table></td>
+</tr>
+</table>
+<?php
+}
+ 
+$sql3="SELECT view FROM ".$tbl_name." WHERE id='".$id."'";
+$result3=mysqli_query($conn,$sql3);
+$rows=mysqli_fetch_array($result3);
+$view=$rows['view'];
+ 
+// if have no counter value set counter = 1
+if(empty($view)){
+$view=1;
+$sql4="INSERT INTO ".$tbl_name."(view) VALUES('".$view."') WHERE id='".$id."'";
+$result4=mysqli_query($conn,$sql4);
+}
+ 
+// count more value
+$addview=$view+1;
+$sql5="update ".$tbl_name." set view='".$addview."' WHERE id='".$id."'";
+$result5=mysqli_query($conn,$sql5);
+mysqli_close($conn);
+?>
+
+<table width="400" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+   <tr>
+      
+    <td>
+        <form name="form1" method="post" action="add_answer.php">
+            <table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+               <tr>
+                  <td width="18%"><strong>Name</strong></td>
+                  <td width="3%">:</td>
+                  <td width="79%"><input name="a_name" type="text" id="a_name" size="45"></td>
+               </tr>
+               <tr>
+                  <td><strong>Email</strong></td>
+                  <td>:</td>
+                  <td><input name="a_email" type="text" id="a_email" size="45"></td>
+               </tr>
+               <tr>
+                  <td valign="top"><strong>Answer</strong></td>
+                  <td valign="top">:</td>
+                  <td><textarea name="a_answer" cols="45" rows="3" id="a_answer"></textarea></td>
+                  /tr>
+               <tr>
+                  <td>&nbsp;</td>
+                  <td><input name="id" type="hidden" value="<?php echo $id; ?>"></td>
+                  <td><input type="submit" name="Submit" value="Submit"> <input type="reset" name="Submit2" value="Reset"></td>
+               </tr>
+            </table>
+        </form>
+    </td>
+      
+   </tr>
+</table>
